37476b9c4e21e2cbc2fbf2e5d64d1c280758a868
bartek-kachnic123 <bartek.kachnic123@gmail.com>
2026-04-09 11:10:51 +0200

    Add login endpoint

--- creational_patterns/src/main/kotlin/com/example/demo/AuthController.kt
+++ creational_patterns/src/main/kotlin/com/example/demo/AuthController.kt
@@ -1,12 +1,14 @@
 package com.example.demo
 
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
 @RequestMapping("/api")
-class AuthController {
+class AuthController(private val authService: AuthService) {
 
     companion object {
         val USERS = listOf(
@@ -18,9 +20,17 @@ class AuthController {
         )
     }
 
+    data class LoginRequest(val username: String, val password: String)
+
     @GetMapping("/users")
     fun getUsers(): List<String> {
         return USERS
+    }
+
+    @PostMapping("/login")
+    fun login(@RequestBody request: LoginRequest): Map<String, Boolean> {
+        val result = authService.authenticate(request.username, request.password)
+        return mapOf("authenticated" to result)
     }
 }
 
